5c7ac1ccfcddb1fea46725fc161d6af5c253e608
Joon-Ho Son <sonjoonh@gmail.com>
2021-05-16 15:10:13 +0100

    Add orthogonal initialisation

--- src/models/acgan/weights.py
+++ src/models/acgan/weights.py
@@ -1,4 +1,6 @@
 import torch
+from torch import nn
+from torch.nn import init
 
 
 def weights_init_normal(m):
@@ -8,3 +10,17 @@ def weights_init_normal(m):
     elif classname.find("BatchNorm") != -1:
         torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
         torch.nn.init.constant_(m.bias.data, 0.0)
+
+
+def weights_init_orthogonal(module):
+    if (
+        isinstance(module, nn.Conv2d)
+        or isinstance(module, nn.ConvTranspose2d)
+        or isinstance(module, nn.Linear)
+    ):
+        init.orthogonal_(module.weight)
+        if module.bias is not None:
+            module.bias.data.fill_(0.0)
+
+    elif isinstance(module, nn.Embedding):
+        init.orthogonal_(module.weight)
