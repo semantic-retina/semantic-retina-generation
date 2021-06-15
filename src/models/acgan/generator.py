2e6d2f7c5765cc78fd8d2874d67d627cc9b8ddc9
Joon-Ho Son <sonjoonh@gmail.com>
2021-06-15 14:42:09 +0100

    Add option to concatenate the label vector

--- src/models/acgan/generator.py
+++ src/models/acgan/generator.py
@@ -5,19 +5,35 @@ from torch import Tensor
 
 class Generator(nn.Module):
     def __init__(
-        self, out_channels: int, img_size: int, n_classes: int, latent_dim: int
+        self,
+        out_channels: int,
+        img_size: int,
+        n_classes: int,
+        latent_dim: int,
+        concat: bool = False,
     ):
         super(Generator, self).__init__()
 
-        self.label_emb = nn.Embedding(n_classes, latent_dim)
+        self.concat = concat
+        if concat:
+            self.label_emb = nn.Embedding(n_classes, n_classes)
+        else:
+            self.label_emb = nn.Embedding(n_classes, latent_dim)
 
         self.init_size = img_size // 2 ** 6  # Initial size before upsampling
 
         self.hidden_channels = 512
 
-        self.l1 = nn.Sequential(
-            nn.Linear(latent_dim, self.hidden_channels * self.init_size ** 2),
-        )
+        if concat:
+            self.l1 = nn.Sequential(
+                nn.Linear(
+                    latent_dim + n_classes, self.hidden_channels * self.init_size ** 2
+                ),
+            )
+        else:
+            self.l1 = nn.Sequential(
+                nn.Linear(latent_dim, self.hidden_channels * self.init_size ** 2),
+            )
 
         self.conv_blocks = nn.Sequential(
             *self.generator_block(self.hidden_channels, self.hidden_channels // 2),
@@ -46,7 +62,12 @@ class Generator(nn.Module):
         ]
 
     def forward(self, noise: Tensor, labels: Tensor):
-        gen_input = torch.mul(self.label_emb(labels), noise)
+
+        if self.concat:
+            gen_input = torch.cat((noise, self.label_emb(labels)), dim=1)
+        else:
+            gen_input = torch.mul(self.label_emb(labels), noise)
+
         out = self.l1(gen_input)
         out = out.view(
             out.shape[0], self.hidden_channels, self.init_size, self.init_size
