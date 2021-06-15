816e2bcd49596588f9205211c18d2b54cc04486c
Joon-Ho Son <sonjoonh@gmail.com>
2021-06-15 14:39:25 +0100

    Return transformed images

--- src/data/datasets/synthetic.py
+++ src/data/datasets/synthetic.py
@@ -13,28 +13,38 @@ from src.data.common import get_label_semantics
 class SyntheticDataset(Dataset):
     """Dataset that returns synthetic data."""
 
-    image_dir = "/vol/bitbucket/js6317/individual-project/SPADE/results/combined-od-hd/test_latest/images/synthesized_image/"
-    label_dir = "/vol/bitbucket/js6317/individual-project/gan-experiment/results/test/acgan-512/label/"
-    inst_dir = "/vol/bitbucket/js6317/individual-project/gan-experiment/results/test/acgan-512/inst/"
-
     def __init__(
         self,
+        name: str = "",
         image_transform: T.Compose = None,
         label_transform: T.Compose = None,
         common_transform: T.Compose = None,
         return_image: bool = True,
+        return_transformed: bool = True,
         return_label: bool = True,
         return_inst: bool = True,
         return_grade: bool = True,
         n_samples: int = -1,
     ):
-        self.image_path = Path(SyntheticDataset.image_dir)
-        self.label_path = Path(SyntheticDataset.label_dir)
-        self.inst_path = Path(SyntheticDataset.inst_dir)
+
+        # TODO: Remove hardcoded paths.
+        image_dir = f"/vol/bitbucket/js6317/individual-project/semantic-dr-gan/results/{name}/test/img/"
+        transformed_dir = f"/vol/bitbucket/js6317/individual-project/semantic-dr-gan/results/{name}/test/transformed/"
+        label_dir = f"/vol/bitbucket/js6317/individual-project/semantic-dr-gan/results/{name}/test/label/"
+        inst_dir = f"/vol/bitbucket/js6317/individual-project/semantic-dr-gan/results/{name}/test/inst/"
+
+        self.image_path = Path(image_dir)
+        self.transformed_path = Path(transformed_dir)
+        self.label_path = Path(label_dir)
+        self.inst_path = Path(inst_dir)
         self.files = []
         self.grades = []
 
         all_files = list(self.label_path.glob("**/*"))
+
+        if len(all_files) == 0:
+            raise FileNotFoundError("dataset not found")
+
         if n_samples == -1:
             subset_files = all_files
         else:
@@ -49,6 +59,7 @@ class SyntheticDataset(Dataset):
         self.common_transform = common_transform
 
         self.return_image = return_image
+        self.return_transformed = return_transformed
         self.return_label = return_label
         self.return_inst = return_inst
         self.return_grade = return_grade
@@ -58,6 +69,10 @@ class SyntheticDataset(Dataset):
 
     def get_image(self, name: str) -> Image:
         image = Image.open(self.image_path / name)
+        return image
+
+    def get_transformed(self, name: str) -> Image:
+        image = Image.open(self.transformed_path / name)
         return image
 
     def get_label(self, name: str) -> Image:
@@ -81,6 +96,14 @@ class SyntheticDataset(Dataset):
             if self.common_transform is not None:
                 image = self.common_transform(image)
             sample["image"] = image
+
+        if self.return_transformed:
+            image = self.get_transformed(filename)
+            if self.image_transform is not None:
+                image = self.image_transform(image)
+            if self.common_transform is not None:
+                image = self.common_transform(image)
+            sample["transformed"] = image
 
         if self.return_label:
             label = self.get_label(filename)
@@ -143,7 +166,3 @@ def test():
     save_image(image, f"test_real.png")
     label[label == 2] = 0.5
     save_image(label, "label.png")
-
-
-if __name__ == "__main__":
-    test()
