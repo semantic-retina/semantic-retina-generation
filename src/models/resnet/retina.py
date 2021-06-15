03678c00198001417ad5b62837bca776078f9b69
Joon-Ho Son <sonjoonh@gmail.com>
2021-06-15 14:46:36 +0100

    Add functions for small retina models

--- src/models/resnet/retina.py
+++ src/models/resnet/retina.py
@@ -19,9 +19,42 @@ def create_retina_model(use_pretrained: bool, feature_extract: bool, n_classes: 
     return DataParallel(model)
 
 
+def create_small_retina_model(
+    use_pretrained: bool,
+    feature_extract: bool,
+    n_classes: int,
+    load_name: str = "",
+):
+    model = models.resnet50(pretrained=use_pretrained)
+    set_parameter_requires_grad(model, feature_extract)
+
+    # Reshape the final layer.
+    fc_n_features = model.fc.in_features
+    model.fc = nn.Linear(fc_n_features, n_classes)
+
+    model = DataParallel(model)
+
+    if load_name:
+        print(f"Loading {load_name}")
+        model.load_state_dict(
+            torch.load(f"results/resnet/{load_name}/checkpoints/model_latest.pth")
+        )
+
+    return model
+
+
 def load_retina_model(path: Path) -> nn.Module:
     """Loads the pre-trained ResNet model for predicting image grades."""
     model = create_retina_model(False, False, 5)
+    model.load_state_dict(torch.load(path))
+    model.eval()
+
+    return model
+
+
+def load_small_retina_model(path: Path) -> nn.Module:
+    """Loads the pre-trained ResNet model for predicting image grades."""
+    model = create_small_retina_model(False, False, 5)
     model.load_state_dict(torch.load(path))
     model.eval()
 
